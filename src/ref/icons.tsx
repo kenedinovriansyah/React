d5a99110d64d615c410f771279b0801a29415bfd
triple-k <triple-k@triple-ks-MacBook-Air.local>
2021-07-29 14:35:17 +0700

    move src folder to desktop

--- src/ref/icons.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Svg, { Props as SvgProps } from 'react-inlinesvg';
-
-export const Icons = React.forwardRef<SVGElement, SvgProps>((props, ref) => (
-  <Svg innerRef={ref} title="icons" {...props} />
-));
--- src/typings/image.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.svg' {
-  let content: any;
-  export default content;
-}
